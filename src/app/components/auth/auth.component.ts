9c851ab06fc60936a8ccaf68fb8c944f7528f49e
Semira gebremikael <semiragere61@gmail.com>
2024-04-08 11:30:41 +0200

    client side validation

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,15 +9,48 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [MatIconModule, MatInputModule, MatButtonModule, CommonModule, MatFormFieldModule, FormsModule],
+  imports: [MatIconModule, MatInputModule, MatButtonModule, CommonModule, MatFormFieldModule, FormsModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
   activeForm: string = 'login';
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    ConfirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
 
   toggleForm(formType: string) {
     this.activeForm = formType;
-
   }
+  onSubmit() {
+    //   if (this.activeForm === 'signup' && this.signupForm.valid) {
+    //     this.authService.register(this.signupForm.value).subscribe({
+    //       next: (response: any) => {
+    //         console.log('User registered successfully', response);
+    //       },
+    //       error: (error: any) => {
+    //         console.error('Registration error', error);
+    //       }
+    //     });
+    //   } else if (this.activeForm === 'login' && this.loginForm.valid) {
+    //     this.authService.login(this.loginForm.value).subscribe({
+    //       next: (response: any) => {
+    //         console.log('User logged in successfully', response);
+    //       },
+    //       error: (error: any) => {
+    //         console.error('Login error', error);
+    //       }
+    //     });
+    //   } else {
+    //     console.log('Form is not valid');
+    //   }
+  }
+
 }
