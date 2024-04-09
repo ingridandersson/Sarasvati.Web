9de58ed971dd38c8318a563971438370e93c227d
Semira gebremikael <semiragere61@gmail.com>
2024-04-09 11:52:49 +0200

    IAuthService

--- src/app/abstract/iuser-service.ts
+++
@@ -1,9 +0,0 @@
-//import { GUID } from "../models/guid.model";
-import { LoginRequest } from "../models/auth/login.request.model";
-//import { LoginResponse } from "../models/auth/login.response.model";
-
-
-export interface IAuthService {
-  login(email: string, password: string): Promise<LoginRequest>;
-  register(email: string, password: string): Promise<LoginRequest>;
-}
