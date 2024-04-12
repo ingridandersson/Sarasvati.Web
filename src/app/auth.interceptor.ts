df7820844eb3b90315afcc2c210df164110ff970
Semira gebremikael <semiragere61@gmail.com>
2024-04-12 15:37:41 +0200

    Interceptor works

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,19 +2,14 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth/auth.service';
-import { Provider } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-@Injectable(
-
-)
+@Injectable()
 
 export class authInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) { };
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    debugger;
     const authToken = this.authService.getAuthToken();
 
     if (authToken) {
@@ -26,15 +21,4 @@ export class authInterceptor implements HttpInterceptor {
     return next.handle(req);
   }
 
-
-  // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  //   const authToken = localStorage.getItem('access_token');
-  //   if (authToken) {
-  //     const authReq = req.clone({
-  //       headers: req.headers.set(`Authorization`, `Bearer ${authToken}`)
-  //     });
-  //     return next.handle(authReq);
-  //   }
-  //   return next.handle(req);
-  // }
 }
